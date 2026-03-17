8133b6bcda2a1651c1e069037e65f74f6e6ea8f0
Alexander Chen <alexanderchen817@gmail.com>
2026-03-17 15:46:26 -0700

    update payload packet naming scheme

--- master/main/main.h
+++ master/main/main.h
@@ -1,18 +1,14 @@
 #pragma once
 #include "esp_err.h"
 #include <stdint.h>
-#include "esp_now.h"
-
-static uint8_t slave_mac_addr[ESP_NOW_ETH_ALEN] = {0x94, 0xE6, 0x86, 0x3B, 0x5D, 0x9C};
-
-static esp_now_peer_info_t peer;
 
 typedef struct {
-    uint8_t up;
-    uint8_t down;
-    uint8_t left;
-    uint8_t right;
-} payload;
+    uint8_t upState;
+    uint8_t downState;
+    uint8_t leftState;
+    uint8_t rightState;
+    uint8_t indexerState;
+} Payload;
 
 void initializeNVS();
 void intializeWifi();
